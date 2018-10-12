32c83901962025dba83c96129fcbfdee555ca4d6
wangxing1212 <44027501+wangxing1212@users.noreply.github.com>
2018-10-12 11:09:05 +0800

    Update data_Structure.m

--- MATLAB/Combine/data_Structure.m
+++ MATLAB/Combine/data_Structure.m
@@ -1,29 +1,29 @@
 function data_Structure(dataPath, savePath, labels, available, classType)
-patientNo = available.patientNo;
-tumourNo = available.tumourNo;
-Edmondson = available.Edmondson;
+patientNo = available.patientNo; % 病人编号
+tumourNo = available.tumourNo;  %图片编号
+Edmondson = available.Edmondson;% 癌症分级
 info = read_Info(patientNo, tumourNo, labels, available);
-% ��ȡmat����
-data0123(dataPath, savePath, info, patientNo, classType, Edmondson, available);% 0123��ȡ���ַֻ�����ͱ���
+% 读取mat数据
+data0123(dataPath, savePath, info, patientNo, classType, Edmondson, available);% 0123读取三种分化区域和背景
 end
 
 function data0123(dataPath, savePath, Info, patientNo, classType, Edmondson, available)
-data_dir = dir(dataPath);
+data_dir = dir(dataPath);% 或者所有文件及子文件
 for i=3:length(data_dir)
     dir_name = data_dir(i).name;
-    if data_dir(i).isdir && strcmp(patientNo, dir_name(1:8))
+    if data_dir(i).isdir && strcmp(patientNo, dir_name(1:8)) %文件夹是否存在，以及与病人编号是否一致
         path = [dataPath data_dir(i).name '/'];
         if strcmp(classType, 'WHO')
-            if ~strcmp('_neg', dir_name(length(dir_name)-3:length(dir_name)))
-                save_train_data012([savePath num2str(Info(5).Level-1) '/'], path, Info, available);%���3��012
+            if ~strcmp('_neg', dir_name(length(dir_name)-3:length(dir_name)))  %后三个与_neg对比
+                save_train_data012([savePath num2str(Info(5).Level-1) '/'], path, Info, available);%存成3类012
             else
-                save_train_data012([savePath '3/'], path, Info, available);%���汳��
+                save_train_data012([savePath '3/'], path, Info, available);%保存背景
             end
         elseif strcmp(classType, 'Edmondson')
             if ~strcmp('_neg', dir_name(length(dir_name)-3:length(dir_name)))
-                save_train_data012([savePath num2str(Edmondson-1) '/'], path, Info, available);%���4��0123
+                save_train_data012([savePath num2str(Edmondson-1) '/'], path, Info, available);%存成4类0123
             else
-                save_train_data012([savePath '4/'], path, Info, available);%���汳��
+                save_train_data012([savePath '4/'], path, Info, available);%保存背景
             end
         end
     end
